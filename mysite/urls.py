9e4125203c3cf173f758134f92196ca11db789b6
kindyaknb <kindyak_nb@fizmat.tnpu.edu.ua>
2024-10-28 13:04:57 +0200

    try4

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import re_path, include
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
-    re_path('', include('blog.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'', include('blog.urls')),
 ]
